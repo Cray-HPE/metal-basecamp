604fa867d9fd4abec3aa5a67109fb4493b9b0dc8
Jacob Salmela <jacob.salmela@hpe.com>
2022-05-09 15:12:19 -0500

    MTL-1319 show errors when basecamp wont start
    Since 0.9.x, basecamp has not shown an error when the service fails to start.  This is due to the layering of services we have in place:
    
    - go application
    - running in a container
    - behind systemd
    - the systemd service just called `podman start`
    - `podman start` returns a `0` status code even if the container failed to start
    - the exit code of the container is also dependent upon whether or not the container started up
    
    To resolve this, moved `podman start` into the `basecamp-init.sh` script.  This required some slight logic adjustments to the code.  To address the last bullet, I do a purge of the existing container before starting one and then I check the error code of that within the metadata.
    
    I also added additional error checking because it's possible to run into a situation where more than one basecamp image exists, and thus the script failed to complete properly.  This accounts for that by removing things and starting a fresh container every time.
    
    Signed-off-by: Jacob Salmela <jacob.salmela@hpe.com>

--- init/basecamp-init.sh
+++ init/basecamp-init.sh
@@ -22,6 +22,7 @@
 # ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
 # OTHER DEALINGS IN THE SOFTWARE.
 #
+set -euf -o pipefail
 
 if [ $# -lt 2 ]; then
     echo >&2 "usage: basecamp-init PIDFILE CIDFILE [CONTAINER [VOLUME]]"
@@ -30,7 +31,8 @@ fi
 
 BASECAMP_PIDFILE="$1"
 BASECAMP_CIDFILE="$2"
-BASECAMP_CONTAINER_NAME="${3-basecamp}"
+BASECAMP_CONTAINER_NAME="${3:-basecamp}"
+BASECAMP_IMAGE_NAME="metal-basecamp"
 
 BASECAMP_IMAGE_PATH="@@basecamp-path@@"
 BASECAMP_IMAGE="@@basecamp-image@@"
@@ -40,9 +42,7 @@ BASECAMP_VOLUME_MOUNT_STATIC='/var/www/ephemeral/static:/app/static:rw,shared'
 
 command -v podman >/dev/null 2>&1 || { echo >&2 "${0##*/}: command not found: podman"; exit 1; }
 
-
-# always ensure pid file is fresh
-rm -f "$BASECAMP_PIDFILE"
+mkdir -pv "$(echo ${BASECAMP_VOLUME_MOUNT_STATIC} | cut -f 1 -d :)"
 mkdir -pv "$(echo ${BASECAMP_VOLUME_MOUNT_CONFIG} | cut -f 1 -d :)"
 test -e "$(echo ${BASECAMP_VOLUME_MOUNT_CONFIG} | cut -f 1 -d :)/data.json" ||\
 cat << EOF > "$(echo ${BASECAMP_VOLUME_MOUNT_CONFIG} | cut -f 1 -d :)/data.json"
@@ -64,28 +64,52 @@ serve-static: true
 static-dir: "./static/"
 EOF
 
-mkdir -pv "$(echo ${BASECAMP_VOLUME_MOUNT_STATIC} | cut -f 1 -d :)"
-# Create basecamp container
-if ! podman inspect "$BASECAMP_CONTAINER_NAME" &>dev/null; then
-    rm -f "$BASECAMP_CIDFILE" || exit
-    # Load basecamp image if it doesn't already exist
-    if ! podman image inspect "$BASECAMP_IMAGE" &>dev/null; then
-        # load the image
-        podman load -i "$BASECAMP_IMAGE_PATH" || exit
-        # get the image id
-        BASECAMP_IMAGE_ID=$(podman images --noheading --format "{{.Id}}" --filter label="org.label-schema.name=$BASECAMP_CONTAINER_NAME")
-        # tag the image
-        podman tag "$BASECAMP_IMAGE_ID" "$BASECAMP_IMAGE"
+
+# Load basecamp image if it doesn't already exist
+if ! podman image inspect "$BASECAMP_IMAGE" &>/dev/null; then
+    # get the image id (there may be more than one in certain situations)
+    BASECAMP_IMAGE_ID=()
+    read -r -a BASECAMP_IMAGE_ID <<< "$(podman images --noheading --format "{{.Id}}" --filter label="org.label-schema.name=$BASECAMP_IMAGE_NAME")"
+
+    # if more than one image is found, clear them out
+    if [ ${#BASECAMP_IMAGE_ID[@]} -gt 1 ]; then
+        podman image rm --force --ignore "${BASECAMP_IMAGE_ID[@]}"
     fi
-    podman create \
-        --conmon-pidfile "$BASECAMP_PIDFILE" \
-        --cidfile "$BASECAMP_CIDFILE" \
-        --cgroups=no-conmon \
-        --net host \
-        --volume $BASECAMP_VOLUME_MOUNT_CONFIG \
-        --volume $BASECAMP_VOLUME_MOUNT_STATIC \
-        --name "$BASECAMP_CONTAINER_NAME" \
-        --env GIN_MODE="${GIN_MODE:-release}" \
-        "$BASECAMP_IMAGE" || exit
-    podman inspect "$BASECAMP_CONTAINER_NAME" || exit
+
+    # otherwise, load and tag the image
+    podman load -i "$BASECAMP_IMAGE_PATH" || exit
+    # get the image ID
+    read -r -a BASECAMP_IMAGE_ID <<< "$(podman images --noheading --format "{{.Id}}" --filter label="org.label-schema.name=$BASECAMP_IMAGE_NAME")"
+    # tag the image appropriately
+    podman tag "${BASECAMP_IMAGE_ID[@]}" "$BASECAMP_IMAGE"
 fi
+
+# always ensure fresh files
+# since this runs in a container and called from a systemd service,
+# the container could have been created with malformed json or some other error
+# clearing it out everytime and making a fresh container allows for a clean start
+podman stop --ignore -t 10 "$BASECAMP_CONTAINER_NAME"
+podman rm --force --ignore "$BASECAMP_CONTAINER_NAME"
+
+rm -f "$BASECAMP_PIDFILE" 
+rm -f "$BASECAMP_CIDFILE"
+
+# create a fresh container in case the previous failed to start due to malformed json
+podman create \
+    --conmon-pidfile "$BASECAMP_PIDFILE" \
+    --cidfile "$BASECAMP_CIDFILE" \
+    --cgroups=no-conmon \
+    --net host \
+    --volume $BASECAMP_VOLUME_MOUNT_CONFIG \
+    --volume $BASECAMP_VOLUME_MOUNT_STATIC \
+    --name "$BASECAMP_CONTAINER_NAME" \
+    --env GIN_MODE="${GIN_MODE:-release}" \
+    "$BASECAMP_IMAGE"
+
+# start the container so we can get the exit code
+podman start "$BASECAMP_CONTAINER_NAME"
+
+# exit this script with the exit code from the container so it is passed appropriately to systemd
+rc=""
+rc=$(podman inspect --type container "${BASECAMP_CONTAINER_NAME}" --format "{{.State.ExitCode}}")
+exit "${rc}"
